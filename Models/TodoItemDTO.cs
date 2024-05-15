9106de94b8d85e609730e81edf60b7c8565b00bf
Alexander Norgren <alexandernorgren2@gmail.com>
2024-05-15 12:50:58 +0200

    Update TodoItemsController.cs, TodoItem.cs, and TodoItemDTO.cs

--- Models/TodoItemDTO.cs
+++ Models/TodoItemDTO.cs
@@ -4,10 +4,10 @@
     {
         public long Id { get; set; }
         public string? Name { get; set; }
-        public bool IsComplete { get; set; }
+        public bool Done { get; set; }
 
         public TodoItemDTO() { }
         public TodoItemDTO(TodoItem todoItem) =>
-            (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
+            (Id, Name, Done) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
     }
 }
